260d2bc26b995462e7f9e5d1cc2be598dd948bd8
Anindya <anindyachakladar9@gmail.com>
2024-11-20 19:34:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import PropTypes from "prop-types";
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { COUNT: 1 };
+    this.state = { COUNT: 1 , num : 2};
   }
 
   render() {
     return (
       <div>
-        <h4>Multiples of: {this.props.OF}</h4>
+        <h4>Multiples of: <input type="text" value={this.state.num} onChange={(e) => {
+     this.setState({ num: e.target.value})}}/></h4>
         <p>
-          {this.props.OF}*{this.state.COUNT}
+          {this.state.num}*{this.state.COUNT} = {parseInt(this.state.num) * this.state.COUNT}
         </p>
         <button onClick={e => this.setState({ COUNT: this.state.COUNT + 1 })}>
           Next Multiple
